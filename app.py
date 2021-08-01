4d36ef2ead0db009924b16b404e41eac45b1ea82
techtalks-crypto <impratikspatil@gmail.com>
2021-08-01 23:02:20 +0530

    new commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from types import TracebackType
+from chatterbot import trainers
+from flask import Flask ,render_template, request
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+app = Flask(__name__)
+
+english_bot =ChatBot("chatterbot", storage_adapter="chatterbot.storage.SQLStorageadApter")
+trainer=ChatterBotCorpusTrainer(english_bot)
+trainer.train("chatterbot.corpus.english")
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+
+@app.route("/get")
+def get_bot_response():
+    userText=request.args.get("msg")
+    return str(english_bot.generate_response(userText))
+
+
+if __name__=='__main__':
+    app.run()
